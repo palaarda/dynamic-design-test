5a69ff821ed4c6c8d9b2a12232717c5c39387d1b
palaarda <gametime.plz@outlook.com>
2023-11-25 20:45:38 +0300

    test2-window.location.href Dosya içi harici html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    // Mobil cihazdan gelen kullanıcıları mobil tasarıma yönlendir
-    document.querySelector('.mobile-design').style.display = 'block';
-    document.querySelector('.desktop-design').style.display = 'none';
+    window.location.href = 'mobile.html';
+  }else{
+    window.location.href = 'index.html';
   }
